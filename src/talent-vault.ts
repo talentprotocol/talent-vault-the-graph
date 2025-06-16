b10000b0b93f14c66443a677e382db1d6e6c98bc
Francisco Leal <francisco@talentprotocol.com>
2025-06-16 16:51:12 +0100

    Track owners and their balances

--- src/talent-vault.ts
+++ src/talent-vault.ts
@@ -5,8 +5,8 @@ import {
   Transfer as TransferEvent,
   Withdraw as WithdrawEvent,
   YieldAccrualDeadlineUpdated as YieldAccrualDeadlineUpdatedEvent,
-  YieldRateUpdated as YieldRateUpdatedEvent
-} from "../generated/TalentVault/TalentVault"
+  YieldRateUpdated as YieldRateUpdatedEvent,
+} from "../generated/TalentVault/TalentVault";
 import {
   Approval,
   Deposit,
@@ -14,38 +14,54 @@ import {
   Transfer,
   Withdraw,
   YieldAccrualDeadlineUpdated,
-  YieldRateUpdated
-} from "../generated/schema"
+  YieldRateUpdated,
+  Owner,
+} from "../generated/schema";
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+
+function getOrCreateOwner(address: Bytes): Owner {
+  let owner = Owner.load(address);
+  if (!owner) {
+    owner = new Owner(address);
+    owner.balance = BigInt.fromI32(0);
+    owner.save();
+  }
+  return owner;
+}
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
+  );
+  entity.owner = event.params.owner;
+  entity.spender = event.params.spender;
+  entity.value = event.params.value;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleDeposit(event: DepositEvent): void {
   let entity = new Deposit(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.owner = event.params.owner
-  entity.assets = event.params.assets
-  entity.shares = event.params.shares
+  );
+  entity.sender = event.params.sender;
+  let owner = getOrCreateOwner(event.params.owner);
+  entity.owner = owner.id;
+  entity.assets = event.params.assets;
+  entity.shares = event.params.shares;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  owner.balance = owner.balance.plus(event.params.shares);
+  owner.save();
 }
 
 export function handleOwnershipTransferred(
@@ -53,47 +69,59 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
+  );
+  let fromOwner = getOrCreateOwner(event.params.from);
+  let toOwner = getOrCreateOwner(event.params.to);
+  entity.from = fromOwner.id;
+  entity.to = toOwner.id;
+  entity.value = event.params.value;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  fromOwner.balance = fromOwner.balance.minus(event.params.value);
+  fromOwner.save();
+
+  toOwner.balance = toOwner.balance.plus(event.params.value);
+  toOwner.save();
 }
 
 export function handleWithdraw(event: WithdrawEvent): void {
   let entity = new Withdraw(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.receiver = event.params.receiver
-  entity.owner = event.params.owner
-  entity.assets = event.params.assets
-  entity.shares = event.params.shares
+  );
+  entity.sender = event.params.sender;
+  entity.receiver = event.params.receiver;
+  let owner = getOrCreateOwner(event.params.owner);
+  entity.owner = owner.id;
+  entity.assets = event.params.assets;
+  entity.shares = event.params.shares;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  owner.balance = owner.balance.minus(event.params.shares);
+  owner.save();
 }
 
 export function handleYieldAccrualDeadlineUpdated(
@@ -101,25 +129,25 @@ export function handleYieldAccrualDeadlineUpdated(
 ): void {
   let entity = new YieldAccrualDeadlineUpdated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.yieldAccrualDeadline = event.params.yieldAccrualDeadline
+  );
+  entity.yieldAccrualDeadline = event.params.yieldAccrualDeadline;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleYieldRateUpdated(event: YieldRateUpdatedEvent): void {
   let entity = new YieldRateUpdated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.yieldRate = event.params.yieldRate
+  );
+  entity.yieldRate = event.params.yieldRate;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
