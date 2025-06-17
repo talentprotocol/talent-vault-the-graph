8f460c151cad50d624ef6167e8db5710a703fb17
Francisco Leal <francisco@talentprotocol.com>
2025-06-17 15:05:09 +0100

    Fix correct calculation of active participants

--- src/talent-vault.ts
+++ src/talent-vault.ts
@@ -1,27 +1,22 @@
 import {
   Deposit as DepositEvent,
-  Transfer as TransferEvent,
   Withdraw as WithdrawEvent,
 } from "../generated/TalentVault/TalentVault";
-import {
-  Deposit,
-  Transfer,
-  Withdraw,
-  Owner,
-  GlobalState,
-} from "../generated/schema";
-import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { Deposit, Withdraw, Owner, GlobalState } from "../generated/schema";
+import { BigInt, Bytes, log } from "@graphprotocol/graph-ts";
 
 const GLOBAL_STATE_ID = "global-state";
+const ZERO_NUMBER = BigInt.fromI32(0);
+const ONE_NUMBER = BigInt.fromI32(1);
 
 function getOrCreateGlobalState(): GlobalState {
   let state = GlobalState.load(GLOBAL_STATE_ID);
   if (!state) {
     state = new GlobalState(GLOBAL_STATE_ID);
-    state.totalDeposits = BigInt.fromI32(0);
-    state.totalWithdraws = BigInt.fromI32(0);
-    state.activeParticipants = BigInt.fromI32(0);
-    state.totalBalance = BigInt.fromI32(0);
+    state.totalDeposits = ZERO_NUMBER;
+    state.totalWithdraws = ZERO_NUMBER;
+    state.activeParticipants = ZERO_NUMBER;
+    state.totalBalance = ZERO_NUMBER;
     state.save();
   }
   return state;
@@ -31,7 +26,7 @@ function getOrCreateOwner(address: Bytes): Owner {
   let owner = Owner.load(address);
   if (!owner) {
     owner = new Owner(address);
-    owner.balance = BigInt.fromI32(0);
+    owner.balance = ZERO_NUMBER;
     owner.save();
   }
   return owner;
@@ -53,55 +48,31 @@ export function handleDeposit(event: DepositEvent): void {
 
   entity.save();
 
-  let oldBalance = owner.balance;
-  owner.balance = owner.balance.plus(event.params.shares);
+  const globalState = getOrCreateGlobalState();
+
+  if (
+    owner.balance.equals(ZERO_NUMBER) &&
+    event.params.assets.gt(ZERO_NUMBER)
+  ) {
+    globalState.activeParticipants =
+      globalState.activeParticipants.plus(ONE_NUMBER);
+  }
+  owner.balance = owner.balance.plus(event.params.assets);
   owner.save();
 
   // Update global state
-  let globalState = getOrCreateGlobalState();
-  globalState.totalDeposits = globalState.totalDeposits.plus(BigInt.fromI32(1));
-  globalState.totalBalance = globalState.totalBalance.plus(event.params.shares);
-  if (
-    oldBalance.equals(BigInt.fromI32(0)) &&
-    event.params.shares.gt(BigInt.fromI32(0))
-  ) {
-    globalState.activeParticipants = globalState.activeParticipants.plus(
-      BigInt.fromI32(1)
-    );
-  }
+  globalState.totalDeposits = globalState.totalDeposits.plus(ONE_NUMBER);
+  globalState.totalBalance = globalState.totalBalance.plus(event.params.assets);
   globalState.save();
 }
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  let fromOwner = getOrCreateOwner(event.params.from);
-  let toOwner = getOrCreateOwner(event.params.to);
-  entity.from = fromOwner.id;
-  entity.to = toOwner.id;
-  entity.value = event.params.value;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-
-  fromOwner.balance = fromOwner.balance.minus(event.params.value);
-  fromOwner.save();
-
-  toOwner.balance = toOwner.balance.plus(event.params.value);
-  toOwner.save();
-}
-
 export function handleWithdraw(event: WithdrawEvent): void {
-  let entity = new Withdraw(
+  const entity = new Withdraw(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   );
   entity.sender = event.params.sender;
   entity.receiver = event.params.receiver;
-  let owner = getOrCreateOwner(event.params.owner);
+  const owner = getOrCreateOwner(event.params.owner);
   entity.owner = owner.id;
   entity.assets = event.params.assets;
   entity.shares = event.params.shares;
@@ -112,26 +83,24 @@ export function handleWithdraw(event: WithdrawEvent): void {
 
   entity.save();
 
-  let oldBalance = owner.balance;
-  owner.balance = owner.balance.minus(event.params.shares);
-  owner.save();
+  const oldBalance = owner.balance;
 
   // Update global state
-  let globalState = getOrCreateGlobalState();
-  globalState.totalWithdraws = globalState.totalWithdraws.plus(
-    BigInt.fromI32(1)
-  );
+  const globalState = getOrCreateGlobalState();
+  globalState.totalWithdraws = globalState.totalWithdraws.plus(ONE_NUMBER);
   globalState.totalBalance = globalState.totalBalance.minus(
     event.params.shares
   );
   if (
-    oldBalance.gt(BigInt.fromI32(0)) &&
-    owner.balance.equals(BigInt.fromI32(0)) &&
-    event.params.shares.gt(BigInt.fromI32(0))
+    oldBalance.gt(ZERO_NUMBER) &&
+    owner.balance.equals(ZERO_NUMBER) &&
+    event.params.assets.gt(ZERO_NUMBER)
   ) {
-    globalState.activeParticipants = globalState.activeParticipants.minus(
-      BigInt.fromI32(1)
-    );
+    globalState.activeParticipants =
+      globalState.activeParticipants.minus(ONE_NUMBER);
   }
   globalState.save();
+
+  owner.balance = owner.balance.minus(event.params.assets);
+  owner.save();
 }
